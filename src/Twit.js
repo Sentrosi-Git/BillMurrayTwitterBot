eabc10f49899e5ba0c9b6f2a4f6f92b37880985b
Bonny <bonny.sentrosi@gmail.com>
2018-06-30 22:18:43 +1000

    filename changes

--- src/Twit.js
+++ src/Twit.js
@@ -1,6 +1,6 @@
 const Twit = require("twit");
 const config = require("../config");
-const Bill = require("./Bill.js")
+// const Bill = require("./Bill.js")
 const T = new Twit(config.twitterApp);
 
 module.exports = T;
