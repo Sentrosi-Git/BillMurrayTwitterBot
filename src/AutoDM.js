c4f9c961acba57362c6003341aa29a2a47927f81
Bonny <bonny.sentrosi@gmail.com>
2018-06-30 23:36:32 +1000

    test filesaving

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -97,62 +97,6 @@ const GenerateMessage = name => {
 
 };
 
-Bill();
+// Bill();
 
 module.exports = AutoDM;
-
-function getPhoto() {
-  const parameters = {
-    url: 'https://api.nasa.gov/planetary/apod',
-    qs: {
-      api_key: process.env.NASA_KEY
-    },
-    encoding: 'binary'
-  }
-  request.get(parameters, (err, respone, body) => {
-    body = JSON.parse(body)
-    saveFile(body, 'nasa.jpg')
-  })
-}
-
-function saveFile(body, fileName) {
-  const file = fs.createWriteStream(fileName)
-  request(body).pipe(file).on('close', err => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('Media saved!')
-      const descriptionText = body.title
-      uploadMedia(descriptionText, fileName)
-    }
-  })
-}
-
-function uploadMedia(descriptionText, fileName) {
-  const filePath = path.join(__dirname, `../${fileName}`)
-  console.log(`file PATH ${filePath}`)
-  bot.postMediaChunked({
-    file_path: filePath
-  }, (err, data, respone) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(data)
-      const params = {
-        status: descriptionText,
-        media_ids: data.media_id_string
-      }
-      postStatus(params)
-    }
-  })
-}
-
-function postStatus(params) {
-  bot.post('statuses/update', params, (err, data, respone) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('Status posted!')
-    }
-  })
-}
